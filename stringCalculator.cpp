7909aa5ea341a9dc9e8e65f3552eabdbad76bf27
maharshi105 <120322375+maharshi105@users.noreply.github.com>
2024-07-30 17:26:26 +0530

    Split Function implemented which can handle comma seprated numbers in string

--- stringCalculator.cpp
+++ stringCalculator.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<string> split(const string &str, const string &delimiter)
+{
+    vector<string> tokens;
+    regex re(delimiter);
+    sregex_token_iterator it(str.begin(), str.end(), re, -1);
+    sregex_token_iterator end;
+    while (it != end)
+    {
+        tokens.push_back(*it++);
+    }
+    return tokens;
+}
+
 int add(const string &numbers)
 {
     if (numbers.empty())
@@ -8,10 +21,17 @@ int add(const string &numbers)
         return 0;
     }
     int sum = 0;
-    int len = numbers.length();
+    string numbersCopy = numbers;
 
-    for(int i = 0; i < len; i++){
-        sum += numbers[i] - '0';
+    vector<string> tokens = split(numbersCopy, ",");
+
+    for (const string &token : tokens)
+    {
+        if (!token.empty())
+        {
+            int num = stoi(token);
+            sum += num;
+        }
     }
 
     return sum;
@@ -21,6 +41,7 @@ int main()
 {
     cout << "Result of empty string: " << add("") << endl;
     cout << "Result of '123': " << add("123") << endl;
+    cout << "Result of '1,2,3': " << add("1,2,3") << endl;
 
     return 0;
 }
