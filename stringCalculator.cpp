e7d2cee1ccca01ffa400d7acff64774502f9eb04
maharshi105 <120322375+maharshi105@users.noreply.github.com>
2024-07-30 17:19:20 +0530

    If only numbers are provided in the string TestCase Handled

--- stringCalculator.cpp
+++ stringCalculator.cpp
@@ -7,10 +7,20 @@ int add(const string &numbers)
     {
         return 0;
     }
+    int sum = 0;
+    int len = numbers.length();
+
+    for(int i = 0; i < len; i++){
+        sum += numbers[i] - '0';
+    }
+
+    return sum;
 }
 
 int main()
 {
     cout << "Result of empty string: " << add("") << endl;
+    cout << "Result of '123': " << add("123") << endl;
+
     return 0;
 }
