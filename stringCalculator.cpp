5fc5daeac6cb60f85db446e86108288df5314b1c
maharshi105 <120322375+maharshi105@users.noreply.github.com>
2024-07-30 17:10:26 +0530

    Empty String TestCase Handled

---
+++ stringCalculator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(const string &numbers)
+{
+    if (numbers.empty())
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    cout << "Result of empty string: " << add("") << endl;
+    return 0;
+}
